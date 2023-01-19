737f2f7f77cd69745c897f65a63ff44b0dae3442
andy.short1011 <andy.short101@gmail.com>
2023-01-19 21:08:38 +0000

    Removed unsued variables

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 import { getAuth, 
     signInWithRedirect,
     signInWithPopup,
@@ -11,14 +11,14 @@ import { getAuth,
 import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBwXFdp37f6gVDGXMWyLwmVXEzXkMkaLRE",
-    authDomain: "crwn-clothing-db-e9197.firebaseapp.com",
-    projectId: "crwn-clothing-db-e9197",
-    storageBucket: "crwn-clothing-db-e9197.appspot.com",
-    messagingSenderId: "562267540587",
-    appId: "1:562267540587:web:7aa0ee31db904e1e61b52e"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBwXFdp37f6gVDGXMWyLwmVXEzXkMkaLRE",
+//     authDomain: "crwn-clothing-db-e9197.firebaseapp.com",
+//     projectId: "crwn-clothing-db-e9197",
+//     storageBucket: "crwn-clothing-db-e9197.appspot.com",
+//     messagingSenderId: "562267540587",
+//     appId: "1:562267540587:web:7aa0ee31db904e1e61b52e"
+// };
   
 // const firebaseApp = initializeApp(firebaseConfig);
 
